34f398f6d277f2558fafbefcc982cdcff472f8e7
tungngt <tung.nguyent03@gmail.com>
2025-05-07 18:28:02 +0700

    focal lost

--- lab3/main.py
+++ lab3/main.py
@@ -35,6 +35,7 @@ def loss(predicts: list[float], targets: list[float]) -> float:
 def derivative_loss(predicts: list[float], targets: list[float]) -> list[float]:
     return [t/p - (1-t)/(1-p) for p, t in zip(predicts, targets)]  
 
+
 def derivative_loss_w0_w1_w2(X1: list[float], X2: list[float], predicts: list[float], targets: list[float]) -> tuple[float, float, float]:
     d_L = derivative_loss(predicts, targets) 
     d_S = derivative_sigmoid(predicts) 
@@ -54,6 +55,46 @@ def derivative_loss_w0_w1_w2(X1: list[float], X2: list[float], predicts: list[fl
     w0 /= -len(predicts)
     w1 /= -len(predicts)
     w2 /= -len(predicts)
+
+    return w0, w1, w2
+
+
+def p_t(predicts: list[float], targets: list[float]) -> list[float]:
+    return [t*p + (1-t)*(1-p) for p, t in zip(predicts, targets)]
+
+def derivative_p_t_p(targets: list[float]) -> list[float]:
+    return [2*t - 1 for t in targets]
+
+
+def focal_loss(gamma: float, predicts: list[float], targets: list[float]) -> float:
+    p_ts = p_t(predicts, targets)
+    return -sum([((1-p_t)**gamma)*math.log(p_t + 5e-5) for p_t in p_ts])/len(predicts)
+
+def derivative_focal_p_t(gamma: float, p_ts: list[float]) -> list[float]:
+    return [gamma*((1-p_t)**(gamma - 1))*math.log(p_t + 5e-5) - ((1-p_t)**gamma)/(p_t + 5e-5) for p_t in p_ts]
+
+
+def derivative_focal_w0_w1_w2(gamma, X1: list[float], X2: list[float], predicts: list[float], targets: list[float]) -> tuple[float, float, float]:
+    p_ts = p_t(predicts, targets)
+    d_F = derivative_focal_p_t(gamma, p_ts) 
+    d_P_T = derivative_p_t_p(targets)
+    d_S = derivative_sigmoid(predicts) 
+    d_W0 = derivative_linear_w0(len(X1))
+    d_W1 = derivative_linear_w1(X1)
+    d_W2 = derivative_linear_w2(X2)
+
+    w0 = 0
+    w1 = 0
+    w2 = 0
+
+    for d_f, d_pt, d_s, d_w0, d_w1, d_w2 in zip(d_F, d_P_T, d_S, d_W0, d_W1, d_W2):
+        w0 += d_f * d_pt * d_s * d_w0
+        w1 += d_f * d_pt * d_s * d_w1
+        w2 += d_f * d_pt * d_s * d_w2
+
+    w0 /= len(predicts)
+    w1 /= len(predicts)
+    w2 /= len(predicts)
 
     return w0, w1, w2
 
@@ -105,6 +146,7 @@ if __name__ == "__main__":
     # X2_norm = X2
     X1_norm, X2_norm, meanx1, meanx2, stdx1, stdx2 = normalize(X1, X2)
 
+    #--------------------------BCE loss----------------------------------
     w0 = 0.01
     w1 = -0.01
     w2 = 0.02
@@ -133,4 +175,36 @@ if __name__ == "__main__":
     x1_draw, x2_draw = draw_line(min(X1_norm), max(X1_norm), w0, w1, w2)
     plt.plot(x1_draw, x2_draw)
     plt.savefig("output_norm.jpg")
+    plt.clf()
+
+    #--------------------------Focal loss----------------------------------
+    w0 = 0.01
+    w1 = -0.01
+    w2 = 0.02
+
+    epochs = 1000
+    lr = 0.1
+    gamma = 0.5
+
+    losses = []
+    for i in range(epochs):
+        predicts = predict(w0, w1, w2, X1_norm, X2_norm)
+        l = focal_loss(gamma, predicts, Y)
+
+        d_w0, d_w1, d_w2 = derivative_focal_w0_w1_w2(gamma, X1_norm, X2_norm, predicts, Y)
+
+        w0 = w0 - lr*d_w0
+        w1 = w1 - lr*d_w1
+        w2 = w2 - lr*d_w2
+
+        losses.append(l)
+
+    plt.plot(losses)
+    plt.savefig("loss_focal.jpg")
+    plt.clf()
+
+    plt.scatter(X1_norm, X2_norm, c=Y, cmap="viridis")
+    x1_draw, x2_draw = draw_line(min(X1_norm), max(X1_norm), w0, w1, w2)
+    plt.plot(x1_draw, x2_draw)
+    plt.savefig("output_norm_focal.jpg")
     plt.clf()
